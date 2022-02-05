68ebdb58a60f0940bf449f549daf7097978184ee
Alex Watts-Barnett <alexrwb@gmail.com>
2022-02-06 00:54:09 +0900

    added repr methods to each class

--- ninpir.py
+++ ninpir.py
@@ -7,16 +7,20 @@ class Ninja:
         self.rank = rank
         self.size = size
     
+    def __repr__(self) -> str:
+        return "This ninja's name is " + self.name + " and they are armed with a " + self.weapon + ". They hold the rank of " + self.rank + '.'
+    
     def be_stealthy(self):
-        stealth_behaviour = ['creeping around.', 'purching up high.', 'hiding in the water.', 'pretending to be a monk.']
-
+        stealthy_behaviour = ['creeping around.', 'purching up high.', 'hiding in the water.', 'pretending to be a monk.']
+        print(self.name + " is " + stealthy_behaviour[random.randint(0, len(stealthy_behaviour) - 1)])
 
     def low_attack(self):
-        (self.name + " attacks low!")
+        (self.name + " attacks low with a " + self.weapon)
     
     def high_attack(self):
-        (self.name + " attacks high!")
-
+        (self.name + " attacks high with a " + self.weapon)
+    def throw_a_ninja_star(self):
+        (self.name + " throws a ninja star.")
     
     
 class Pirate:
@@ -25,19 +29,33 @@ class Pirate:
         self.weapon = weapon
         self.rank = rank
         self.size = size
+    
+    def __repr__(self) -> str:
+        return "This pirate's name is " + self.name + " and they are armed with a " + self.weapon + ". They hold the rank of " + self.rank + '.'
 
     def say_pirate_stuff(self):
         pirate_stuff = ['Arrrr', 'Shiver me timbers!', 'Blow me down!', 'Yo ho ho and a bottle of rum!']
-        print(self.name + " yells: " + pirate_stuff[random.randint(0, len(pirate_stuff) - 1)])
+        print(self.name + " yells " + '"' + pirate_stuff[random.randint(0, len(pirate_stuff) - 1)] + '"')
 
     def low_attack(self):
-        print(self.name + " attacks low!")
+        print(self.name + " attacks low with a " + self.weapon)
     
     def high_attack(self):
-        print(self.name + " attacks high!")
+        print(self.name + " attacks high with a " + self.weapon)
     
     def fire_pistol(self):
         print(self.name + "fired a shot!")
+
+
+ninja_one = Ninja('Hitori Hanzo', 'ninjato', 'ninja master', 'medium')
+
+ninja_one.be_stealthy()
+print(ninja_one.__repr__())
+
+pirate_one = Pirate('Long John Silver', 'cutlass', 'pirate captain', 'medium')
+
+pirate_one.say_pirate_stuff()
+print(pirate_one.__repr__())
 
 
 
